641df2bab19ddaece3ad196bb63f07ec6547fecd
Kurtis Eberhardt <77414699+KurtisEberhardt@users.noreply.github.com>
2021-03-05 21:00:41 -0700

    fixed :)

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -9,11 +9,15 @@ export default class Post {
   }
 
   get Template() {
-    return `            <div class="col-11">
+    return /* html */ `
     <div class="border-round card p-2 card-opacity">
         <div class="card-title p-2">
-            <div class="ml-5">${this.title}</div>
-            <div class="ml-5">${this.description}</div>
+            <div class="ml-5">
+                ${this.title}
+            </div>
+            <div class="ml-5">
+                ${this.description}
+            </div>
             <div class="text mx-5">
             </div>
             <div class="col-1">
@@ -25,31 +29,35 @@ export default class Post {
                 </div>
             </div>
         </div>
-        <div class="card-body">
-            <!-- <div class="skeleton-loader text block"></div>
-                <div class="skeleton-loader text block"></div> -->
-            <span>Forgiveness</span>
-            <div class="progress">
-                <div id="progressbar" class="progress-bar bg-success" style="width: 0%" aria-valuenow="25"
-                    aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="row">
+            <div class="col-11">
+                <div class="card-body">
+                    <span>Forgiveness</span>
+                    <div class="progress">
+                        <div id="progressbar" class="progress-bar bg-success" style="width: 0%" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <button class="btn btn-primary mt-3" type="button" this-toggle="collapse"
+                        data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Comments
+                    </button>
+                    <div class="collapse" id="collapseExample">
+                        <form action="" class="form-group"
+                            onsubmit="app.commentsController.createComment(event, '${this.id}','${this.id}')">
+                            <p><input type="text" placeholder="Make Comment?" id="description"
+                                    class="input-lg col-lg-6">
+                            </p>
+                            <button class="btn btn-primary" type="submit">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                ${this.Comments}
             </div>
-            <button class="btn btn-primary mt-3" type="button" this-toggle="collapse"
-                data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Comments
-            </button>
-            <div class="collapse" id="collapseExample">
-            <form action="" class="form-group"
-            onsubmit="app.commentsController.createComment(event, '${this.id}','${this.id}')">
-            <p><input type="text" placeholder="Make Comment?" id="description"
-                    class="input-lg col-lg-6">
-            </p>
-            <button class="btn btn-primary" type="submit">Submit</button>
-        </form>
-
+        </div>
     </div>
-            ${this.Comments}
-
-</div>`
+`
   }
 
   get Comments() {
